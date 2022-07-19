46b026333824511a416fc606b1c20f865f668e9f
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2022-07-19 11:55:52 +0200

    setup welcome page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+	<div>
 		<div v-if="!showPlayArea" class="game-container">
 			<h1>Welcome to Snake Game</h1>
 			<button @click="handleShowPlayerArea">Start Game</button>
@@ -8,30 +8,61 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent, ref } from "vue";
 export default defineComponent({
-  name: "WelcomePage",
-  props: {
-    msg: String,
-  },
+	name: "WelcomePage",
+	props: {
+		msg: String,
+	},
+	components: {
+		
+	},
+	setup() {
+		const showPlayerNamePrompt = ref(false);
+		const showPlayArea = ref(false);
+		const playerName = ref("");
+		const handleShowPlayerPromptClick = () => {
+			showPlayerNamePrompt.value = true;
+		};
+		const handleShowPlayerArea = () => {
+			showPlayerNamePrompt.value = false;
+			showPlayArea.value = true;
+		};
+		return {
+			showPlayerNamePrompt,
+			showPlayArea,
+			playerName,
+			handleShowPlayerPromptClick,
+			handleShowPlayerArea,
+		};
+	},
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.game-container {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	margin: 20px auto;
+	border: 5px solid rgb(224, 108, 13);
+	border-radius: 5px;
+	background-color: #33cc00;
+	height: 30vh;
+	width: 50vw;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+button {
+	padding: 10px 20px;
+	border: none;
+	background-color: rgb(100, 6, 72);
+	border-radius: 5px;
+	color: #fff;
+	cursor: pointer;
+	width: 50%;
 }
 </style>
