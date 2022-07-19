0f0aedc74664987224c5d5ff2da28178d0a53f42
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2022-07-19 11:31:00 +0200

    Implement basic welcome page sturcture

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+		<div v-if="!showPlayArea" class="game-container">
+			<h1>Welcome to Snake Game</h1>
+			<button @click="handleShowPlayerArea">Start Game</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "WelcomePage",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
